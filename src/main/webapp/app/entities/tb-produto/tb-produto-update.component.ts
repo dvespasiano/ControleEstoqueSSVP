d21fa76f2ec98212ca3c075d06ccb0d10eb502a8
João Pedro Brier <43352736+JPBrier@users.noreply.github.com>
2019-11-17 20:09:19 -0300

    Listar Categorias no select - Adicionar Produto

--- src/main/webapp/app/entities/tb-produto/tb-produto-update.component.ts
+++ src/main/webapp/app/entities/tb-produto/tb-produto-update.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -6,20 +6,29 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
 import { filter, map } from 'rxjs/operators';
-import { JhiAlertService } from 'ng-jhipster';
+import { JhiAlertService, JhiEventManager } from 'ng-jhipster';
 import { ITbProduto, TbProduto } from 'app/shared/model/tb-produto.model';
 import { TbProdutoService } from './tb-produto.service';
 import { ITbMovimentacao } from 'app/shared/model/tb-movimentacao.model';
 import { TbMovimentacaoService } from 'app/entities/tb-movimentacao/tb-movimentacao.service';
 
+import { Subscription } from 'rxjs';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { ITbCategoria } from 'app/shared/model/tb-categoria.model';
+import { AccountService } from 'app/core/auth/account.service';
+import { TbCategoriaService } from '../tb-categoria/tb-categoria.service';
+
 @Component({
   selector: 'jhi-tb-produto-update',
   templateUrl: './tb-produto-update.component.html'
 })
-export class TbProdutoUpdateComponent implements OnInit {
+export class TbProdutoUpdateComponent implements OnInit, OnDestroy {
   isSaving: boolean;
 
   tbmovimentacaos: ITbMovimentacao[];
+  tbCategorias: ITbCategoria[];
+  currentAccount: any;
+  eventSubscriber: Subscription;
 
   editForm = this.fb.group({
     id: [],
@@ -36,11 +45,19 @@ export class TbProdutoUpdateComponent implements OnInit {
     protected tbProdutoService: TbProdutoService,
     protected tbMovimentacaoService: TbMovimentacaoService,
     protected activatedRoute: ActivatedRoute,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    protected tbCategoriaService: TbCategoriaService,
+    protected eventManager: JhiEventManager,
+    protected accountService: AccountService
   ) {}
 
   ngOnInit() {
     this.isSaving = false;
+    this.loadAll();
+    this.accountService.identity().then(account => {
+      this.currentAccount = account;
+    });
+    this.registerChangeInTbCategorias();
     this.activatedRoute.data.subscribe(({ tbProduto }) => {
       this.updateForm(tbProduto);
     });
@@ -119,5 +136,32 @@ export class TbProdutoUpdateComponent implements OnInit {
       }
     }
     return option;
+  }
+
+  loadAll() {
+    this.tbCategoriaService
+      .query()
+      .pipe(
+        filter((res: HttpResponse<ITbCategoria[]>) => res.ok),
+        map((res: HttpResponse<ITbCategoria[]>) => res.body)
+      )
+      .subscribe(
+        (res: ITbCategoria[]) => {
+          this.tbCategorias = res;
+        },
+        (res: HttpErrorResponse) => this.onError(res.message)
+      );
+  }
+
+  ngOnDestroy() {
+    this.eventManager.destroy(this.eventSubscriber);
+  }
+
+  trackId(index: number, item: ITbCategoria) {
+    return item.id;
+  }
+
+  registerChangeInTbCategorias() {
+    this.eventSubscriber = this.eventManager.subscribe('tbCategoriaListModification', response => this.loadAll());
   }
 }
