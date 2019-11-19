d179849b64f85e4494919044a0d15567866778b8
DedeCefet <43896714+DedeCefet@users.noreply.github.com>
2019-11-19 18:20:43 -0300

    Atualizaão de Roles
    Atualização de roles para administradores e usuários

--- src/main/webapp/app/entities/tb-categoria/tb-categoria.route.ts
+++ src/main/webapp/app/entities/tb-categoria/tb-categoria.route.ts
@@ -33,7 +33,7 @@ export const tbCategoriaRoute: Routes = [
     path: '',
     component: TbCategoriaComponent,
     data: {
-      authorities: ['ROLE_USER'],
+      authorities: ['ROLE_ADMIN'],
       pageTitle: 'TbCategorias'
     },
     canActivate: [UserRouteAccessService]
@@ -45,7 +45,7 @@ export const tbCategoriaRoute: Routes = [
       tbCategoria: TbCategoriaResolve
     },
     data: {
-      authorities: ['ROLE_USER'],
+      authorities: ['ROLE_ADMIN'],
       pageTitle: 'TbCategorias'
     },
     canActivate: [UserRouteAccessService]
@@ -57,7 +57,7 @@ export const tbCategoriaRoute: Routes = [
       tbCategoria: TbCategoriaResolve
     },
     data: {
-      authorities: ['ROLE_USER'],
+      authorities: ['ROLE_ADMIN'],
       pageTitle: 'TbCategorias'
     },
     canActivate: [UserRouteAccessService]
@@ -69,7 +69,7 @@ export const tbCategoriaRoute: Routes = [
       tbCategoria: TbCategoriaResolve
     },
     data: {
-      authorities: ['ROLE_USER'],
+      authorities: ['ROLE_ADMIN'],
       pageTitle: 'TbCategorias'
     },
     canActivate: [UserRouteAccessService]
